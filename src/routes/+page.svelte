4e0621893ec964a630ecbffabfbd7449ab18dd32
Marco Matteo <marco.matteo2007@icloud.com>
2024-10-31 21:42:56 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    
+</script>
+
+<div class="main">
+    <div class="linkBox">
+        <div class="link">
+            <img src="https://i.scdn.co/image/ab67616d0000b273a1e867d40e7bb29ced5c0194" alt="dielit" class="link-image">
+            <h4>Die Lit</h4>
+        </div>
+        <div class="link">
+            <img src="https://i.scdn.co/image/ab67616d0000b273a1e867d40e7bb29ced5c0194" alt="litdie" class="link-image">
+            <h4>Lit Die</h4>
+        </div>
+        <div class="link">
+            <img src="https://i.scdn.co/image/ab67616d0000b273a1e867d40e7bb29ced5c0194" alt="liedit" class="link-image">
+            <h4>Lie Dit</h4>
+        </div>
+    </div>
+</div>
+
+<style>
+    .linkBox {
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    .link {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .link-image {
+        width: 15rem;
+        height: 10rem;
+    }
+</style>
